4a0cd8f358fb8f99fbbb5380587cb4c07e3d746d
MSS <sarmadsaleem.333@gmail.com>
2024-04-02 22:42:23 +0500

    Added mui

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button } from "@mui/material";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React App</h1>
+        <h1>Search O Pal Assesment Form</h1>
+        <Button variant="contained">Hello world</Button>
       </header>
     </div>
   );
