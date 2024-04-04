2d255aa8af4bffbb498ae9bc2ef27c8493cbe28a
MSS <sarmadsaleem.333@gmail.com>
2024-04-04 20:04:31 +0500

    Completed Login and SignUp page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
 import Login from "./components/Login";
+import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </BrowserRouter>
       </header>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -14,172 +14,255 @@ import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import InputLabel from "@mui/material/InputLabel";
-
+import * as Yup from "yup";
+import { Formik, Form } from "formik";
 const theme = createTheme();
+
+const SignUpSchema = Yup.object().shape({
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string().required("Required"),
+
+  companyName: Yup.string().when("activeForm", {
+    is: "Employer",
+    then: Yup.string().required("Required"),
+  }),
+  country: Yup.string().required("Required"),
+  contactNumber: Yup.string().required("Required"),
+  // .matches(/^\d{11}$/, "Contact number must be exactly 11 digits"),
+});
 
 export default function SignUp() {
   const [activeForm, setActiveForm] = useState("JobSeeker");
 
   const changeForm = (value) => {
-    switch (value) {
-      case "JobSeeker":
-        setActiveForm("JobSeeker");
-        break;
-      case "Employer":
-        setActiveForm("Employer");
-        break;
-      case "Trainer":
-        setActiveForm("Trainer");
-        break;
-      default:
-        setActiveForm("JobSeeker");
-    }
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      name: data.get("name"),
-      country: data.get("country"),
-      email: data.get("email"),
-      contactNumber: data.get("contactNumber"),
-      password: data.get("password"),
-    });
+    setActiveForm(value);
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: theme.spacing(3),
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            bgcolor: "background.paper",
-            boxShadow: theme.shadows[3],
-            borderRadius: theme.shape.borderRadius,
-            padding: theme.spacing(3),
-          }}
-        >
-          <Typography component="h6" variant="h6">
-            Create {activeForm} Account
-          </Typography>
+    <Formik
+      initialValues={{
+        email: "",
+        password: "",
+        name: "",
+        companyName: "",
+        country: "",
+        contactNumber: "",
+        activeForm: "JobSeeker",
+      }}
+      validationSchema={SignUpSchema}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
+    >
+      {({
+        errors,
+        touched,
+        values,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+      }) => (
+        <Form>
+          <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: theme.spacing(3),
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  bgcolor: "background.paper",
+                  boxShadow: theme.shadows[3],
+                  borderRadius: theme.shape.borderRadius,
+                  padding: theme.spacing(3),
+                }}
+              >
+                <Typography component="h6" variant="h6">
+                  Create {activeForm} Account
+                </Typography>
 
-          <Grid container spacing={3} justifyContent="center">
-            <Grid item xs={12} sm={4}>
-              <Button
-                variant={activeForm === "JobSeeker" ? "contained" : "outlined"}
-                onClick={() => changeForm("JobSeeker")}
-              >
-                JobSeeker
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Button
-                variant={activeForm === "Employer" ? "contained" : "outlined"}
-                onClick={() => changeForm("Employer")}
-              >
-                Employer
-              </Button>
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Button
-                variant={activeForm === "Trainer" ? "contained" : "outlined"}
-                onClick={() => changeForm("Trainer")}
-              >
-                Trainer
-              </Button>
-            </Grid>
-          </Grid>
-          <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <form onSubmit={handleSubmit} noValidate>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <TextField
-                  autoComplete="given-name"
-                  name="name"
-                  required
-                  fullWidth
-                  id="name"
-                  label="Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <InputLabel id="country-label">Country</InputLabel>
-                  <Select
-                    labelId="country-label"
-                    id="country"
-                    name="country"
-                    label="Country"
+                <Grid container spacing={3} justifyContent="center">
+                  <Grid item xs={12} sm={4}>
+                    <Button
+                      variant={
+                        activeForm === "JobSeeker" ? "contained" : "outlined"
+                      }
+                      onClick={() => changeForm("JobSeeker")}
+                    >
+                      JobSeeker
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Button
+                      variant={
+                        activeForm === "Employer" ? "contained" : "outlined"
+                      }
+                      onClick={() => changeForm("Employer")}
+                    >
+                      Employer
+                    </Button>
+                  </Grid>
+                  <Grid item xs={12} sm={4}>
+                    <Button
+                      variant={
+                        activeForm === "Trainer" ? "contained" : "outlined"
+                      }
+                      onClick={() => changeForm("Trainer")}
+                    >
+                      Trainer
+                    </Button>
+                  </Grid>
+                </Grid>
+                <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <form onSubmit={handleSubmit} noValidate>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                      {activeForm === "Employer" ? (
+                        <TextField
+                          autoComplete="given-name"
+                          name="companyName"
+                          required
+                          fullWidth
+                          id="companyName"
+                          label="Company Name"
+                          autoFocus
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.companyName}
+                          error={errors.companyName && touched.companyName}
+                          helperText={
+                            errors.companyName &&
+                            touched.companyName &&
+                            errors.companyName
+                          }
+                        />
+                      ) : (
+                        <TextField
+                          autoComplete="given-name"
+                          name="name"
+                          required
+                          fullWidth
+                          id="name"
+                          label="Name"
+                          autoFocus
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.name}
+                          error={errors.name && touched.name}
+                          helperText={
+                            errors.name && touched.name && errors.name
+                          }
+                        />
+                      )}
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <InputLabel id="country-label">Country</InputLabel>
+                        <Select
+                          labelId="country-label"
+                          id="country"
+                          name="country"
+                          label="Country"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.country}
+                          error={errors.country && touched.country}
+                          helperText={
+                            errors.country && touched.country && errors.country
+                          }
+                        >
+                          <MenuItem value="USA">USA</MenuItem>
+                          <MenuItem value="Canada">Canada</MenuItem>
+                          <MenuItem value="UK">UK</MenuItem>
+                          <MenuItem value="Pakistan">Pakistan</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        error={errors.email && touched.email}
+                        helperText={
+                          errors.email && touched.email && errors.email
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="phone"
+                        label="Contact Number"
+                        type="number"
+                        name="contactNumber"
+                        autoComplete="tel"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.contactNumber}
+                        error={errors.contactNumber && touched.contactNumber}
+                        helperText={
+                          errors.contactNumber &&
+                          touched.contactNumber &&
+                          errors.contactNumber
+                        }
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.password}
+                        error={errors.password && touched.password}
+                        helperText={
+                          errors.password && touched.password && errors.password
+                        }
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                    sx={{ mt: theme.spacing(3), mb: theme.spacing(2) }}
                   >
-                    <MenuItem value="USA">USA</MenuItem>
-                    <MenuItem value="Canada">Canada</MenuItem>
-                    <MenuItem value="UK">UK</MenuItem>
-                    <MenuItem value="Pakistan">Pakistan</MenuItem>
-                  </Select>
-                </FormControl>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="phone"
-                  label="Contact Number"
-                  type="number"
-                  name="contactNumber"
-                  autoComplete="tel"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              sx={{ mt: theme.spacing(3), mb: theme.spacing(2) }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="center">
-              <Grid item>
-                <Link href="/login" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </Box>
-      </Container>
-    </ThemeProvider>
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="center">
+                    <Grid item>
+                      <Link href="/login" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </form>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </Form>
+      )}
+    </Formik>
   );
 }
