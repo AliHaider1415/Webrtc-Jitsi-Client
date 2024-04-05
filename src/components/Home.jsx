1f8dcf859529e87fe06fcece7fe95e7707e676c7
MSS <sarmadsaleem.333@gmail.com>
2024-04-05 13:59:58 +0500

    Added home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 export default function Home() {
-  return <div>Hello World from MSS!</div>;
+  return <div className="Navbar"></div>;
 }
