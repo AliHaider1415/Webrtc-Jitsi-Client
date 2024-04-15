2fd121dd0c88fb8badb80f9f6559814f5a9d80ef
MSS <sarmadsaleem.333@gmail.com>
2024-04-16 00:22:57 +0500

    Added sign up integreation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -40,11 +40,12 @@ export default function Login() {
   const showToastMessage = (message) => {
     console.log(message);
     if (message.error) {
-      toast.error(message.error);
+      toast.error(message.error, {});
     } else {
-      toast.success(message.message);
+      toast.success(message.message, {});
+      localStorage.setItem("access", message.access);
       setTimeout(() => {
-        navigate("/"); // Navigate to the home page after a delay
+        navigate("/");
       }, 2000);
     }
   };
@@ -171,3 +172,194 @@ export default function Login() {
     </div>
   );
 }
+
+// import * as React from "react";
+// import Avatar from "@mui/material/Avatar";
+// import Button from "@mui/material/Button";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import TextField from "@mui/material/TextField";
+// import Link from "@mui/material/Link";
+// import Grid from "@mui/material/Grid";
+// import Box from "@mui/material/Box";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+// import Typography from "@mui/material/Typography";
+// import Container from "@mui/material/Container";
+// import { createTheme, ThemeProvider } from "@mui/material/styles";
+// import { Formik, Form } from "formik";
+// import { LoginSchema } from "../utils/Schemas";
+// import { ToastContainer, toast } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import { useNavigate } from "react-router-dom";
+// import { useMutation } from "react-query";
+
+// function Copyright(props) {
+//   return (
+//     <Typography
+//       variant="body2"
+//       color="text.secondary"
+//       align="center"
+//       {...props}
+//     >
+//       {"Copyright © "}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{" "}
+//       {new Date().getFullYear()}
+//       {"."}
+//     </Typography>
+//   );
+// }
+
+// const defaultTheme = createTheme();
+
+// export default function Login() {
+//   const navigate = useNavigate();
+
+//   const showToastMessage = (message) => {
+//     console.log(message);
+//     if (message.error) {
+//       toast.error(message.error, {});
+//     } else {
+//       toast.success(message.message, {});
+//       localStorage.setItem("access", message.access);
+//       setTimeout(() => {
+//         navigate("/");
+//       }, 2000);
+//     }
+//   };
+
+//   const loginMutation = useMutation(async (values) => {
+//     const response = await fetch("http://127.0.0.1:8000/accounts/login", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         email: values.email,
+//         password: values.password,
+//       }),
+//     });
+//     const json = await response.json();
+//     return json;
+//   });
+
+//   const handleLogin = async (values) => {
+//     try {
+//       const data = await loginMutation.mutateAsync(values);
+//       showToastMessage(data);
+//     } catch (error) {
+//       console.error("An error occurred:", error);
+//       toast.error("An error occurred while logging in");
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Formik
+//         initialValues={{
+//           email: "",
+//           password: "",
+//         }}
+//         validationSchema={LoginSchema}
+//         onSubmit={(values) => {
+//           handleLogin(values);
+//         }}
+//       >
+//         {({
+//           errors,
+//           touched,
+//           values,
+//           handleChange,
+//           handleBlur,
+//           handleSubmit,
+//         }) => (
+//           <Form>
+//             <ThemeProvider theme={defaultTheme}>
+//               <Container component="main" maxWidth="xs">
+//                 <CssBaseline />
+//                 <Box
+//                   sx={{
+//                     marginTop: 8,
+//                     display: "flex",
+//                     flexDirection: "column",
+//                     alignItems: "center",
+//                     bgcolor: "background.paper",
+//                     boxShadow: 3,
+//                     padding: 3,
+//                     borderRadius: 1,
+//                   }}
+//                 >
+//                   <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+//                     <LockOutlinedIcon />
+//                   </Avatar>
+//                   <Typography component="h1" variant="h5">
+//                     Sign in
+//                   </Typography>
+//                   <Box component="form" noValidate sx={{ mt: 1 }}>
+//                     <TextField
+//                       margin="normal"
+//                       required
+//                       fullWidth
+//                       id="email"
+//                       label="Email Address"
+//                       name="email"
+//                       onChange={handleChange}
+//                       onBlur={handleBlur}
+//                       value={values.email}
+//                       error={errors.email && touched.email}
+//                       helperText={errors.email && touched.email && errors.email}
+//                       autoComplete="email"
+//                       autoFocus
+//                     />
+//                     <TextField
+//                       margin="normal"
+//                       required
+//                       fullWidth
+//                       name="password"
+//                       label="Password"
+//                       onChange={handleChange}
+//                       onBlur={handleBlur}
+//                       value={values.password}
+//                       error={errors.password && touched.password}
+//                       helperText={
+//                         errors.password && touched.password && errors.password
+//                       }
+//                       type="password"
+//                       id="password"
+//                       autoComplete="current-password"
+//                     />
+
+//                     <Button
+//                       type="submit"
+//                       fullWidth
+//                       variant="contained"
+//                       sx={{ mt: 3, mb: 2 }}
+//                       onClick={handleSubmit}
+//                       disabled={loginMutation.isLoading}
+//                     >
+//                       {loginMutation.isLoading ? "Signing in..." : "Sign In"}
+//                     </Button>
+//                     <Grid container>
+//                       <Grid item xs>
+//                         <Link href="#" variant="body2">
+//                           Forgot password?
+//                         </Link>
+//                       </Grid>
+//                       <Grid item>
+//                         <Link href="/sign-up" variant="body2">
+//                           {"Don't have an account? Sign Up"}
+//                         </Link>
+//                       </Grid>
+//                     </Grid>
+//                   </Box>
+//                 </Box>
+//                 <Copyright sx={{ mt: 8, mb: 4 }} />
+//               </Container>
+//             </ThemeProvider>
+//           </Form>
+//         )}
+//       </Formik>
+//       <ToastContainer />
+//     </div>
+//   );
+// }
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -14,226 +14,285 @@ import { Formik, Form } from "formik";
 import CountrySelect from "./CountrySelect";
 import countries from "../utils/countries";
 import { SignUpSchema } from "../utils/Schemas";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 const theme = createTheme();
 
 export default function SignUp() {
+  const navigate = useNavigate();
+  const showToastMessage = (message) => {
+    console.log(message);
+    if (message.error) {
+      toast.error(message.error, {});
+    } else {
+      toast.success("Account Created Successfully", {});
+      localStorage.setItem("access", message.access);
+      setTimeout(() => {
+        navigate("/");
+      }, 2000);
+    }
+  };
   const [activeForm, setActiveForm] = useState("JobSeeker");
 
   const changeForm = (value) => {
     setActiveForm(value);
   };
 
-  return (
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-        name: "",
-        country: "",
-        phone: "",
-      }}
-      validationSchema={SignUpSchema}
-      onSubmit={(values) => {
-        console.log(values);
-      }}
-    >
-      {({
-        errors,
-        touched,
-        values,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-      }) => (
-        <Form>
-          <ThemeProvider theme={theme}>
-            <Container component="main" maxWidth="xs">
-              <CssBaseline />
-              <Box
-                sx={{
-                  marginTop: theme.spacing(3),
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "center",
-                  bgcolor: "background.paper",
-                  boxShadow: theme.shadows[3],
-                  borderRadius: theme.shape.borderRadius,
-                  padding: theme.spacing(3),
-                }}
-              >
-                <Typography component="h6" variant="h6">
-                  Create {activeForm} Account
-                </Typography>
+  const handleSignUp = async (values) => {
+    let apiUrl;
+    switch (activeForm) {
+      case "Employer":
+        apiUrl = "http://127.0.0.1:8000/company/company-signup";
+        break;
+      case "JobSeeker":
+        apiUrl = "http://127.0.0.1:8000/candidates/user-signup";
+        break;
+      case "Trainer":
+        apiUrl = "http://127.0.0.1:8000/candidates/user-signup";
+        break;
+      default:
+        break;
+    }
 
-                <Grid container spacing={3} justifyContent="center">
-                  <Grid item xs={12} sm={4}>
-                    <Button
-                      variant={
-                        activeForm === "JobSeeker" ? "contained" : "outlined"
-                      }
-                      onClick={() => changeForm("JobSeeker")}
-                    >
-                      JobSeeker
-                    </Button>
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: values.email,
+        name: values.name,
+        phone: values.phone,
+        password: values.password,
+        country: values.country,
+        is_candidate: activeForm === "JobSeeker",
+        is_employer: activeForm === "Employer",
+        is_trainer: activeForm === "Trainer",
+      }),
+    });
+
+    const json = await response.json();
+    showToastMessage(json);
+  };
+
+  return (
+    <div>
+      {" "}
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+          name: "",
+          country: "",
+          phone: "",
+        }}
+        validationSchema={SignUpSchema}
+        onSubmit={(values) => {
+          handleSignUp(values);
+        }}
+      >
+        {({
+          errors,
+          touched,
+          values,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <Form>
+            <ThemeProvider theme={theme}>
+              <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                  sx={{
+                    marginTop: theme.spacing(3),
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    bgcolor: "background.paper",
+                    boxShadow: theme.shadows[3],
+                    borderRadius: theme.shape.borderRadius,
+                    padding: theme.spacing(3),
+                  }}
+                >
+                  <Typography component="h6" variant="h6">
+                    Create {activeForm} Account
+                  </Typography>
+
+                  <Grid container spacing={3} justifyContent="center">
+                    <Grid item xs={12} sm={4}>
+                      <Button
+                        variant={
+                          activeForm === "JobSeeker" ? "contained" : "outlined"
+                        }
+                        onClick={() => changeForm("JobSeeker")}
+                      >
+                        JobSeeker
+                      </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                      <Button
+                        variant={
+                          activeForm === "Employer" ? "contained" : "outlined"
+                        }
+                        onClick={() => changeForm("Employer")}
+                      >
+                        Employer
+                      </Button>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                      <Button
+                        variant={
+                          activeForm === "Trainer" ? "contained" : "outlined"
+                        }
+                        onClick={() => changeForm("Trainer")}
+                      >
+                        Trainer
+                      </Button>
+                    </Grid>
                   </Grid>
-                  <Grid item xs={12} sm={4}>
-                    <Button
-                      variant={
-                        activeForm === "Employer" ? "contained" : "outlined"
-                      }
-                      onClick={() => changeForm("Employer")}
-                    >
-                      Employer
-                    </Button>
-                  </Grid>
-                  <Grid item xs={12} sm={4}>
-                    <Button
-                      variant={
-                        activeForm === "Trainer" ? "contained" : "outlined"
-                      }
-                      onClick={() => changeForm("Trainer")}
-                    >
-                      Trainer
-                    </Button>
-                  </Grid>
-                </Grid>
-                <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
-                  <LockOutlinedIcon />
-                </Avatar>
-                <Typography component="h1" variant="h5">
-                  Sign up
-                </Typography>
-                <form onSubmit={handleSubmit} noValidate>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      {activeForm === "Employer" ? (
-                        <TextField
-                          autoComplete="given-name"
-                          name="name"
-                          required
-                          fullWidth
-                          id="name"
-                          label="Company Name"
-                          autoFocus
+                  <Avatar sx={{ m: 1, bgcolor: theme.palette.secondary.main }}>
+                    <LockOutlinedIcon />
+                  </Avatar>
+                  <Typography component="h1" variant="h5">
+                    Sign up
+                  </Typography>
+                  <form onSubmit={handleSubmit} noValidate>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        {activeForm === "Employer" ? (
+                          <TextField
+                            autoComplete="given-name"
+                            name="name"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Company Name"
+                            autoFocus
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.name}
+                            error={errors.name && touched.name}
+                            helperText={
+                              errors.name && touched.name && errors.name
+                            }
+                          />
+                        ) : (
+                          <TextField
+                            autoComplete="given-name"
+                            name="name"
+                            required
+                            fullWidth
+                            id="name"
+                            label="Name"
+                            autoFocus
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.name}
+                            error={errors.name && touched.name}
+                            helperText={
+                              errors.name && touched.name && errors.name
+                            }
+                          />
+                        )}
+                      </Grid>
+                      <Grid item xs={12}>
+                        <CountrySelect
+                          countries={countries}
+                          id="country"
+                          name="country"
+                          label="Country"
                           onChange={handleChange}
                           onBlur={handleBlur}
-                          value={values.name}
-                          error={errors.name && touched.name}
+                          value={values.country}
+                          error={errors.country && touched.country}
                           helperText={
-                            errors.name && touched.name && errors.name
+                            errors.country && touched.country && errors.country
                           }
                         />
-                      ) : (
+                      </Grid>
+                      <Grid item xs={12}>
                         <TextField
-                          autoComplete="given-name"
-                          name="name"
                           required
                           fullWidth
-                          id="name"
-                          label="Name"
-                          autoFocus
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
                           onChange={handleChange}
                           onBlur={handleBlur}
-                          value={values.name}
-                          error={errors.name && touched.name}
+                          value={values.email}
+                          error={errors.email && touched.email}
                           helperText={
-                            errors.name && touched.name && errors.name
+                            errors.email && touched.email && errors.email
                           }
                         />
-                      )}
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          id="phone"
+                          type="tel"
+                          label="Contact Number"
+                          name="phone"
+                          autoComplete="tel"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.phone}
+                          error={errors.phone && touched.phone}
+                          helperText={
+                            errors.phone && touched.phone && errors.phone
+                          }
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="new-password"
+                          onChange={handleChange}
+                          onBlur={handleBlur}
+                          value={values.password}
+                          error={errors.password && touched.password}
+                          helperText={
+                            errors.password &&
+                            touched.password &&
+                            errors.password
+                          }
+                        />
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <CountrySelect
-                        countries={countries}
-                        id="country"
-                        name="country"
-                        label="Country"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.country}
-                        error={errors.country && touched.country}
-                        helperText={
-                          errors.country && touched.country && errors.country
-                        }
-                      />
+                    <Button
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      onClick={handleSubmit}
+                      sx={{ mt: theme.spacing(3), mb: theme.spacing(2) }}
+                    >
+                      Sign Up
+                    </Button>
+                    <Grid container justifyContent="center">
+                      <Grid item>
+                        <Link href="/login" variant="body2">
+                          Already have an account? Sign in
+                        </Link>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        error={errors.email && touched.email}
-                        helperText={
-                          errors.email && touched.email && errors.email
-                        }
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="phone"
-                        type="tel"
-                        label="Contact Number"
-                        name="phone"
-                        autoComplete="tel"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.phone}
-                        error={errors.phone && touched.phone}
-                        helperText={
-                          errors.phone && touched.phone && errors.phone
-                        }
-                      />
-                    </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        type="password"
-                        id="password"
-                        autoComplete="new-password"
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                        error={errors.password && touched.password}
-                        helperText={
-                          errors.password && touched.password && errors.password
-                        }
-                      />
-                    </Grid>
-                  </Grid>
-                  <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    onClick={handleSubmit}
-                    sx={{ mt: theme.spacing(3), mb: theme.spacing(2) }}
-                  >
-                    Sign Up
-                  </Button>
-                  <Grid container justifyContent="center">
-                    <Grid item>
-                      <Link href="/login" variant="body2">
-                        Already have an account? Sign in
-                      </Link>
-                    </Grid>
-                  </Grid>
-                </form>
-              </Box>
-            </Container>
-          </ThemeProvider>
-        </Form>
-      )}
-    </Formik>
+                  </form>
+                  <ToastContainer />
+                </Box>
+              </Container>
+            </ThemeProvider>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
